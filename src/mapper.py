03122a2a713ff56b69d06a512bcb142bbad2564e
Amid Majd <amidmajd@gmail.com>
2021-11-22 22:44:11 +0330

    reducer with sorted output + mapper small fixes

--- src/mapper.py
+++ src/mapper.py
@@ -9,7 +9,7 @@ STOP_WORDS = ['hasn', 'few', "it's", 'before', 'while', 'be', 'up', 'haven', 'wi
               'once', 'then', 'her', 'at', "don't", 'most', 'what', "you'll", 'such', 'very', 'o', 'too',
               'after', 'been', "hasn't", 'don', 'ma', 'me', 'we', 'into', "haven't", 'than', "she's",
               'didn', 'd', 'themselves', 'are', 'his', 's', "shan't", 'against', 'weren', 'how', 'my',
-              'doesn', 'herself', 'or', 'y', 'so', 'she', 'all', 'both', 'hadn', 'not',
+              'doesn', 'herself', 'or', 'y', 'so', 'she', 'all', 'both', 'hadn', 'not', 'many', ''
               'couldn', 'same', 'through', "you're", 'ain', 'just', "aren't", 'off', "mightn't", 'he',
               'will', 'you', 'our', 'its', 'should', "should've", 'who', 'the', 'have', 'of', 'do',
               'these', 'until', 'above', 'was', 'doing', "needn't", 'that', 'himself', 'own', 'were',
@@ -18,7 +18,7 @@ STOP_WORDS = ['hasn', 'few', "it's", 'before', 'while', 'be', 'up', 'haven', 'wi
               'some', 'mightn', 'mustn', 'down', 'can', "wasn't", 're', 'there', 'isn', 'their', 'to',
               'those', 'under', 'does', 'shouldn', 'ours', 'them', 't', 'll', 'this', 'by', "isn't",
               'itself', 'why', 'below', 'again', "you've", "didn't", 'your', 'being', 'yourselves', 'yours',
-              'any', 'yourself', 'has', 've', 'hers']
+              'any', 'yourself', 'has', 've', 'hers', 'us', 'would', 'may', 'maybe']
 
 
 def remove_chars(string, list_of_chars):
@@ -28,8 +28,8 @@ def remove_chars(string, list_of_chars):
 
 
 def clean_word(word):
-    # returns cleaned word or None if input is not a propper word
-    word = remove_chars(word, list("\"':;,./\\<>()-=!#$%^&*~“”…•")).lower()
+    # returns cleaned word or None or empty string if input is not a propper word
+    word = remove_chars(word, list("\"':;,./\\<>\{}()-–_=!#$%^&*~“”…•" + '0123456789')).lower()
     if word not in STOP_WORDS:
         return word
     else:
@@ -54,4 +54,3 @@ def main(separator='\t'):
 
 if __name__ == "__main__":
     main()
-    # print(clean_word('"assd"'))
--- src/reducer.py
+++ src/reducer.py
@@ -3,6 +3,10 @@
 from itertools import groupby
 from operator import itemgetter
 import sys
+import collections
+
+
+counter = collections.Counter()
 
 
 def read_mapper_output(file, separator='\t'):
@@ -14,17 +18,13 @@ def main(separator='\t'):
     # input comes from STDIN (standard input)
     data = read_mapper_output(sys.stdin, separator=separator)
 
-    # groupby: generates a break or new group every time the value of the key function changes
-    # groupby bellow returns a word as key AND all of it's Continuous separate counts as value as a list
-    # so for this to work keys need to be sorted first
-    # which is done automatically by hadoop's internal shuffle & sort when running mapreduce
-    for current_word, word_counts_list in groupby(data, itemgetter(0)):
+    for word, count in data:
         try:
-            total_count = sum(int(count) for current_word, count in word_counts_list)
-            print(current_word, total_count, sep=separator)
+            counter[word] += int(count)
         except ValueError:
-            # if count was not a number don't count it
             pass
+    for word, count in counter.most_common():
+        print(word, count, sep=separator)
 
 
 if __name__ == "__main__":
